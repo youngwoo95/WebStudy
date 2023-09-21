b2edc75e40ac86e7333fb663305cec3485091009
youngwoo95 <rladyddn258@gmail.com>
2023-09-21 23:14:52 +0900

    weather API 사용해보기 / HTTP 프로토콜 통신 (get) / then ~ catch 사용방법

--- 자바스크립트/ToDo-list/script.js
+++ 자바스크립트/ToDo-list/script.js
@@ -1,24 +1,34 @@
 const todoInput = document.querySelector("#todo-input");
 const todolist = document.querySelector("#todo-list");
 
-const createTodo = function () {
+const saveTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+const createTodo = function (storageData) {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.content;
+  }
+
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
   const newBtn = document.createElement("button");
 
   newBtn.addEventListener("click", () => {
     newLi.classList.toggle("complete");
+    saveItemsFn();
   });
 
   newLi.addEventListener("dblclick", () => {
     newLi.remove();
+    saveItemsFn();
   });
 
-  console.log(newBtn);
+  if (storageData?.complete) {
+    newLi.classList.add("complete");
+  }
 
-  newSpan.textContent = todoInput.value;
+  newSpan.textContent = todoContents;
   newLi.appendChild(newBtn);
-
   newLi.appendChild(newSpan);
   todolist.appendChild(newLi);
   todoInput.value = "";
@@ -37,6 +47,7 @@ const deleteAll = function () {
   for (let i = 0; i < liList.length; i++) {
     liList[i].remove();
   }
+  saveItemsFn();
 };
 
 const saveItemsFn = function () {
@@ -49,5 +60,63 @@ const saveItemsFn = function () {
     };
     saveItems.push(todoObj);
   }
-  console.log(saveItems);
+
+  if (saveItems.length === 0) {
+    localStorage.removeItem("saved-items");
+  } else {
+    // console.log(String(saveItems));
+    console.log(JSON.stringify(saveItems)); // JSON 변환
+    localStorage.setItem("saved-items", JSON.stringify(saveItems));
+  }
 };
+
+if (saveTodoList) {
+  for (let i = 0; i < saveTodoList.length; i++) {
+    createTodo(saveTodoList[i]);
+  }
+}
+
+const accessToGeo = function (position) {
+  const positionObj = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+  };
+  console.log(positionObj);
+
+  weatherSearch(positionObj);
+};
+
+const askForLocation = function () {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (error) => {
+    console.log(error);
+  });
+};
+askForLocation();
+
+const weatherSearch = function (position) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${position.latitude}&lon=${position.longitude}&appid=d20c89b987c68c75aed8dd66fee8c34a`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json.name, json.weather[0].description);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+// const promiseTest = function () {
+//   return new Promise((resolver, reject) => {
+//     setTimeout(() => {
+//       resolver("success"); // 성공했을 때 사용
+//       // reject("error"); // 실패했을 때 사용
+//     }, 5000);
+//   });
+// };
+
+// promiseTest().then((res) => {
+//   console.log(res);
+// });
